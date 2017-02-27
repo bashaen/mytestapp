b9a8fc7f38b9270f429f26e547fd9221c8aa6cb7
IceAndrewBSD <Andrew.Ice@e-hps.com>
2017-02-27 11:31:07 -0500

    Update database

--- db.php
+++ db.php
@@ -2,7 +2,7 @@
 
 try {
         // $db = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";port=" . DB_PORT, DB_USER, DB_PASS);
-        $db = new PDO("mysql:host=localhost;dbname=homeck;port=3306", "root", "root");
+        $db = new PDO("mysql:host=138.197.88.253;dbname=homec;port=3306", "root", "Cupcak3s123!");
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $db->exec("SET NAMES 'utf8'");
     } catch ( Exception $e ) {

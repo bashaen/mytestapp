aabbff4d61c0656095060cbbc1b85076bbf2f390
IceAndrewBSD <Andrew.Ice@e-hps.com>
2017-02-27 19:15:29 -0500

    Update chores call

--- chores/getAllHouseholdChores.php
+++ chores/getAllHouseholdChores.php
@@ -1,14 +1,13 @@
 <?php
-    header('Access-Control-Allow-Origin: *');
-    require("../check-session.php");
-    require("../db.php");
-//    $home_id = $_GET['homeId'];
-    $home_id = '1';
-    $chores_complete_incomplete = [];
-    $chores_active_inactive = [];
+require("../check-session.php");
+require("../db.php");
+$home_id = $_GET['homeId'];
+$home_id = '1';
+$chores_complete_incomplete = [];
+$chores_active_inactive = [];
 
-    try {
-        $results = $db->query("
+try {
+    $results = $db->query("
             SELECT
                 homeck.chores.chore_id as chore_id,
                 homeck.chores.title as chore_name,
@@ -26,15 +25,15 @@
 		          WHERE homeck.chores_users.home_id = $home_id
 		          AND homeck.chores_users.active = 1
 		          AND homeck.chores_users.completed = 1;");
-    } catch (Exception $e) {
-        echo("Could not get Home 1");
-        exit;
-    }
-    $active_complete = $results->fetchAll(PDO::FETCH_ASSOC);
-    $chores_complete_incomplete["complete"] = $active_complete;
+} catch (Exception $e) {
+    echo("Could not get Home");
+    exit;
+}
+$active_complete = $results->fetchAll(PDO::FETCH_ASSOC);
+$chores_complete_incomplete["complete"] = $active_complete;
 
-    try {
-        $results = $db->query("
+try {
+    $results = $db->query("
                 SELECT
                     homeck.chores.chore_id as chore_id,
                     homeck.chores.title as chore_name,
@@ -52,15 +51,15 @@
                         WHERE homeck.chores_users.home_id = $home_id
                         AND homeck.chores_users.active = 1
                         AND homeck.chores_users.completed = 0;");
-    } catch (Exception $e) {
-        echo("Could not get Home 2");
-        exit;
-    }
-    $active_incomplete = $results->fetchAll(PDO::FETCH_ASSOC);
-    $chores_complete_incomplete["incomplete"] = $active_incomplete;
+} catch (Exception $e) {
+    echo("Could not get Home");
+    exit;
+}
+$active_incomplete = $results->fetchAll(PDO::FETCH_ASSOC);
+$chores_complete_incomplete["incomplete"] = $active_incomplete;
 
-    try {
-        $results = $db->query("
+try {
+    $results = $db->query("
                     SELECT
                         homeck.chores.chore_id as chore_id,
                         homeck.chores.title as chore_name,
@@ -77,11 +76,11 @@
                             ON homeck.chores_users.user_id = homeck.users.user_id
                             WHERE homeck.chores_users.home_id = $home_id
                             AND homeck.chores_users.active = 0");
-    } catch (Exception $e) {
-        echo("Could not get Home 3");
-        exit;
-    }
-    $inactive_chores["all"] = $results->fetchAll(PDO::FETCH_ASSOC);
-    $chores_active_inactive["active"] = $chores_complete_incomplete;
-    $chores_active_inactive["inactive"] = $inactive_chores;
-    echo(json_encode($chores_active_inactive));
+} catch (Exception $e) {
+    echo("Could not get Home");
+    exit;
+}
+$inactive_chores["all"] = $results->fetchAll(PDO::FETCH_ASSOC);
+$chores_active_inactive["active"] = $chores_complete_incomplete;
+$chores_active_inactive["inactive"] = $inactive_chores;
+echo(json_encode($chores_active_inactive));
